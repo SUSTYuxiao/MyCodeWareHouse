7f365c1f2d817aab959440589c18b7b5ddbe5114
张鹏霄 <zpx736312737@126.com>
2018-04-27 21:08:49 +0800

    bitmap&bloomFilter
    add bitMap
    add bloomFilter

--- BitMap.h
+++ BitMap.h
@@ -5,19 +5,42 @@
 
 #ifndef MYCODEWAREHOUSE_BITMAP_H
 #define MYCODEWAREHOUSE_BITMAP_H
+
 #include <cstdio>
-template <class K>
+#include <vector>
+
 class BitMap
 {
     public:
-    BitMap(size_t num)
+    BitMap(size_t range)
     {
-
+        _bit.resize((range >> 5) + 1, 0);
     }
-    void Set(const K &key)
+
+    void Set(const size_t num)
     {
-
+        size_t whichByte = num >> 5; //whichByte 指位图中哪一个size_t存储该位
+        size_t whichBit = num % 32;
+        _bit[whichByte] |= 1 << whichBit;
     }
+
+    void ReSet(const size_t num)
+    {
+        size_t whichByte = num >> 5; //whichByte 指位图中哪一个size_t存储该位
+        size_t whichBit = num % 32;
+        _bit[whichByte] &= ~(1 << whichBit);
+    }
+
+    bool Find(const size_t num)
+    {
+        size_t whichByte = num >> 5; //whichByte 指位图中哪一个size_t存储该位
+        size_t whichBit = num % 32;
+        return (bool)(_bit[whichByte] & (1 << whichBit));
+    }
+
+    protected:
+    std::vector<size_t> _bit;
+
 };
 
 #endif //MYCODEWAREHOUSE_BITMAP_H
--- BloomFilter.h
+++ BloomFilter.h
@@ -1,6 +1,9 @@
 //
 // Created by 张鹏霄 on 2018/4/24.
 // 布隆过滤器
+// 布隆过滤器
+// 认为某个值存在是不准确的，
+// 认为某个值不存在是准确的，
 //
 
 #ifndef MYCODEWAREHOUSE_BLOOMFILTER_H
@@ -8,31 +11,56 @@
 
 #include "BitMap.h"
 
-template <class K>
+template <class K
+        ,class HashFuc1
+        ,class HashFuc2
+        ,class HashFuc3>
 class BloomFilter
 {
     public:
-    BloomFilter(size_t num):_bm(num*5) //总位数 = (k / 0.7) * 数据个数 k为映射几个位
-    {
+    BloomFilter(size_t num)
+            :_bm(num * 5)
+            ,_range(num * 5)
+    {}//总位数 = (k / 0.7) * 数据个数 k为映射几个位
 
+    bool Set(const K& key)
+    {
+        //使用HashFuc*()(key) 通过使用匿名对象的()重载，避免声明一个普通对象
+        size_t hash1nd = HashFuc1()(key);
+        size_t hash2nd = HashFuc2()(key);
+        size_t hash3nd = HashFuc3()(key);
+
+        _bm.Set(hash1nd % _range);
+        _bm.Set(hash2nd % _range);
+        _bm.Set(hash3nd % _range);
     }
-    void Set(const K& key)
+
+//    void ReSet(const K &key)
+//    {
+//
+//    }
+
+    bool Find(const K &key)
     {
+        size_t hash1nd = HashFuc1()(key);
+        size_t hash2nd = HashFuc2()(key);
+        size_t hash3nd = HashFuc3()(key);
 
-    }
+        if(! _bm.Find(hash1nd % _range)) //find返回值为0 则说明没找到
+            return false;
 
-    void ReSet(const K &key)
-    {
+        if(! _bm.Find(hash2nd % _range))
+            return false;
 
-    }
+        if(! _bm.Find(hash3nd % _range))
+            return false;
 
-    void Test(const K &key)
-    {
-
+        return true;
     }
 
     protected:
     BitMap _bm;
+    size_t _range;
 };
 
 #endif //MYCODEWAREHOUSE_BLOOMFILTER_H
